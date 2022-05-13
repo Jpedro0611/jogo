d6284d602e71f15a7ab0f416729cb061017c3085
Jpedro0611 <joaopedrolimabernardo@gmail.com>
2022-05-13 18:46:27 -0300

    .

--- sketch.js
+++ sketch.js
@@ -7,6 +7,9 @@ var shotgp;
 var countErv = 0;
 var life = 3;
 var cor;
+var estadojogo = 'jogar';
+var controle = true;
+var limite;
 
 function preload(){ // função que carregar todas as imagens e animações
    bg = loadImage ("assets/bg2.png")
@@ -29,14 +32,18 @@ function setup(){ // todas as configuraçoes dos objetos
   girassol.scale = 0.6
   cor = createSprite(485,80,360,150)
   cor.shapeColor = 'SaddleBrown'
-  
+
+  limite = createSprite(10,600,20,1300)
+  limite.visible = false;
   shotgp = new Group();
   ervilhas = new Group();
   gpsol = new Group();
   zbgp = new Group();
+
 }
 
 function draw(){
+  if (estadojogo === 'jogar'){
   background(bg);
   textSize(80);
   fill('black');
@@ -57,7 +64,27 @@ function draw(){
   createsol();
   newscore();
   createzombie();
-  //ervilha();
+  ervilha();
+  if (zbgp.isTouching (limite)){
+    estadojogo = 'gameOver'
+
+
+
+  }
+ if (frameCount%100 === 0){
+  controle = true;
+
+ }
+  
+  if (life === 0){
+    estadojogo = 'gameOver'
+
+
+  }
+  }else if(estadojogo === 'gameOver'){
+    GameOver();
+  }
+  
 }
 
 function Destroyer (ervilha,zb){
@@ -81,7 +108,7 @@ zb.destroy();
 
 function keyboard(){
  if (keyDown (DOWN_ARROW) ){
-    if (sol_score/100 >=1){
+    if (sol_score/100 >=1 && controle == true){
       var p1 = createSprite(mouseX,mouseY,20,20);
       sol_score -= 100;
       p1.addImage('p1',p1_img);
@@ -90,6 +117,7 @@ function keyboard(){
       countErv ++;
       p1.setCollider('circle',0,0,350);
       ervilhas.add(p1);
+      controle = false;
     }
   } 
 }
@@ -122,10 +150,10 @@ function newscore(){ // aumenta a pontuação do sol
 }
 
 function createzombie (){
-  if (frameCount%250 === 0){
+  if (frameCount%150 === 0){
   var zb 
-  zb = createSprite(1250,random(250,450,1050),20,20)
-  zb.velocityX = -1;
+  zb = createSprite(1250,random(250,1050),20,20)
+  zb.velocityX = -3;
   var randy = Math.round(random(1,6));
   switch(randy){
     case 1:zb.addImage('zombie',zb1);
@@ -143,7 +171,7 @@ function createzombie (){
   }
   zb.scale = 1.5
   zb.lifetime = 1300;
- // zb.debug = true;
+  zb.debug = true;
   zbgp.add(zb);
   }
   
@@ -163,18 +191,23 @@ if (frameCount%60 === 0){
     shot.scale = 2;
     shot.lifetime = 600;
     shotgp.add(shot);
-
+    shot.debug = true;
+    shot.setCollider('circle',0,0,20)
   }
-  
+ 
 
 }
 
 
 }
 
-// function ervilha (){
-// TE = createSprite(p1.x,p1.y,10,10);
-
-
-
-// }
+function GameOver () {
+  background('black');
+  stroke('white')
+  strokeWeight('15')
+  fill('black');
+  textSize(80);
+  text('GAME OVER',400,600)
+  image(zb1,200,800,200,200)
+  image(zb2,800,300,200,200)
+}
